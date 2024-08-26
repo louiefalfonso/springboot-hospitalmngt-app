4ca62bf27d9131628784b18243479bdd7805a487
Louie Alfonso <lfriasalfonso@gmail.com>
2024-08-26 16:51:48 +0100

    Fixed CORS Issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />          
+          <Route path="/signup" element={<Register />} />          
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/appointments" element={<AppoinmentsPage />} />
           <Route path="/appointments/:id" element={<AppoinmentPage />} />
