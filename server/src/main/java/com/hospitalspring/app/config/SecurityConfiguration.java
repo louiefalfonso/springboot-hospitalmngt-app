1b72134aab828a20d248220fbb263bb43afbba55
Louie Alfonso <79123140+louiefalfonso@users.noreply.github.com>
2024-08-26 16:25:05 +0100

    Update SecurityConfiguration.java

--- server/src/main/java/com/hospitalspring/app/config/SecurityConfiguration.java
+++ server/src/main/java/com/hospitalspring/app/config/SecurityConfiguration.java
@@ -54,8 +54,8 @@ public class SecurityConfiguration {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:8080"));
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("https://springboot-hospitalmngt-app.onrender.com"));
+        configuration.setAllowedOrigins(List.of("https://springboot3-stlukesapp.netlify.app/"));
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE"));
         configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
 
