810eb7212151fa7b9080c78d768fd5cb4d62473b
Louie Alfonso <lfriasalfonso@gmail.com>
2024-08-27 14:39:28 +0100

    Configure CORS Issues

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,43 +1,39 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import dotenv from "dotenv";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 dotenv.config();
 
 const baseUriDev = process.env.VITE_BASE_URI_DEV;
+
+const apiProxy = createProxyMiddleware({
+  target: baseUriDev,
+  changeOrigin: true,
+  pathRewrite: { "^/api": "", "^/auth": "" },
+  onProxyRes: (proxyRes) => {
+    console.log("Proxy response:", proxyRes);
+    proxyRes.headers["Access-Control-Allow-Origin"] = "*";
+    proxyRes.headers["Access-Control-Allow-Methods"] =
+      "GET, POST, PUT, DELETE, OPTIONS";
+    proxyRes.headers["Access-Control-Allow-Headers"] =
+      "Content-Type, Authorization";
+  },
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      headers: ["Content-Type", "Authorization"],
+    },
     proxy: {
-      "/api": {
-        target: baseUriDev,
-        changeOrigin: true,
-        secure: false,
-        cors: true,
-        configure: (proxy, req, res) => {
-          res.setHeader("Access-Control-Allow-Origin", "*");
-          res.setHeader(
-            "Access-Control-Allow-Headers",
-            "Origin, X-Requested-With, Content-Type, Accept"
-          );
-        },
-      },
-      "/auth": {
-        target: baseUriDev,
-        changeOrigin: true,
-        secure: false,
-        cors: true,
-        configure: (proxy, req, res) => {
-          res.setHeader("Access-Control-Allow-Origin", "*");
-          res.setHeader(
-            "Access-Control-Allow-Headers",
-            "Origin, X-Requested-With, Content-Type, Accept"
-          );
-        },
-      },
+      "/api": apiProxy,
+      "/auth": apiProxy,
     },
   },
 });
