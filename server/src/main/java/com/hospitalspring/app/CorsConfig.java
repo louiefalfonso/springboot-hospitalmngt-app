546b6197fd8a8e8a35a0ae1f25c72f49cc4d657a
Louie Alfonso <lfriasalfonso@gmail.com>
2024-08-27 16:31:44 +0100

    Configure CORS Issue

--- server/src/main/java/com/hospitalspring/app/CorsConfig.java
+++ server/src/main/java/com/hospitalspring/app/CorsConfig.java
@@ -7,15 +7,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("https://springboot3-stlukesapp.netlify.app/",
-                                "https://springboot-hospitalmngt-app.onrender.com/")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") 
+                        .allowedOrigins("https://springboot3-stlukesapp.netlify.app/**",
+                                "https://springboot-hospitalmngt-app.onrender.com/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .maxAge(3600);
+
+                registry.addMapping("/auth/**")
+                        .allowedOrigins("https://springboot3-stlukesapp.netlify.app/**",
+                                "https://springboot-hospitalmngt-app.onrender.com/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .maxAge(3600);
             }
         };
