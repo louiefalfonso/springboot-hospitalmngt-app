9bfa1a0635d3c0527ca41bb7b841ff7962d97c50
Louie Alfonso <lfriasalfonso@gmail.com>
2024-08-27 17:15:47 +0100

    Configure CORS Issue

--- server/src/main/java/com/hospitalspring/app/CorsConfig.java
+++ server/src/main/java/com/hospitalspring/app/CorsConfig.java
@@ -1,7 +1,9 @@
 package com.hospitalspring.app;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
--- server/src/main/java/com/hospitalspring/app/HospitalappApplication.java
+++ server/src/main/java/com/hospitalspring/app/HospitalappApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
@@ -19,6 +21,30 @@ public class HospitalappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HospitalappApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**")
+						.allowedOrigins("https://springboot3-stlukesapp.netlify.app",
+								"https://springboot-hospitalmngt-app.onrender.com")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")
+						.allowedHeaders("*")
+						.exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Methods", "Access-Control-Allow-Headers") // Add this line
+						.maxAge(3600);
+
+				registry.addMapping("/auth/**")
+						.allowedOrigins("https://springboot3-stlukesapp.netlify.app",
+								"https://springboot-hospitalmngt-app.onrender.com")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD")
+						.allowedHeaders("*")
+						.exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Methods", "Access-Control-Allow-Headers") // Add this line
+						.maxAge(3600);
+			}
+		};
 	}
 
 
