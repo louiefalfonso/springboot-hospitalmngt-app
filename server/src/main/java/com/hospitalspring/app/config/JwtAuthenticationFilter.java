9ca2fa82026e5d5f037753c9938c82930ede70f3
Louie Alfonso <lfriasalfonso@gmail.com>
2024-08-27 19:00:26 +0100

    Configure CORS Issue

--- server/src/main/java/com/hospitalspring/app/config/JwtAuthenticationFilter.java
+++ server/src/main/java/com/hospitalspring/app/config/JwtAuthenticationFilter.java
@@ -43,6 +43,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
+
+        if (request.getMethod().equals("OPTIONS")) {
+            response.setStatus(HttpServletResponse.SC_OK);
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         final String authHeader = request.getHeader("Authorization");
 
         if (authHeader == null || !authHeader.startsWith("Bearer")) {
--- server/src/main/java/com/hospitalspring/app/config/SecurityConfiguration.java
+++ server/src/main/java/com/hospitalspring/app/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.hospitalspring.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -35,6 +36,7 @@ public class SecurityConfiguration {
                 .and()
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/api/**").permitAll()
                         .anyRequest().authenticated()
